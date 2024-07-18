ca22bd04428dbd8f69f8b05ced8adb38b532aa5e
LSK <seulki-2>
2024-07-18 14:26:08 +0900

    seulki_sbjoin,Controller, signup

--- src/main/java/kr/soft/study/HomeController.java
+++ src/main/java/kr/soft/study/HomeController.java
@@ -35,6 +35,64 @@ public class HomeController {
 		
 		return "home";
 	}
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		System.out.println("signup");
+		return "signup";
+	}
 	
+	
+	@RequestMapping("/sbjoin")
+	public String sbjoin(Model model) {
+		System.out.println("sbjoin");
+		return "sbjoin";
+	}
+	
+	@RequestMapping("/sbtransfer")
+	public String sbexperience(Model model) {
+		System.out.println("sbtransfer");
+		return "sbtransfer";
+	}
+	@RequestMapping("/sbsavings")
+	public String sbsavings(Model model) {
+		System.out.println("sbtransfer");
+		return "sbsavings";
+	}
+	@RequestMapping("/sbfund")
+	public String sbfund(Model model) {
+		System.out.println("sbtransfer");
+		return "sbfund";
+	}
+	
+	@RequestMapping("/ibjoin")
+	public String ibjoin(Model model) {
+		System.out.println("ibjoin");
+		return "ibjoin";
+	}
+	@RequestMapping("/ibtransfer")
+	public String ibexperience(Model model) {
+		System.out.println("ibtransfer");
+		return "ibtransfer";
+	}
+	@RequestMapping("/ibsavings")
+	public String ibsavings(Model model) {
+		System.out.println("ibtransfer");
+		return "ibsavings";
+	}
+	@RequestMapping("/ibfund")
+	public String ibfund(Model model) {
+		System.out.println("ibtransfer");
+		return "ibfund";
+	}
+	@RequestMapping("/quiz")
+	public String quiz(Model model) {
+		System.out.println("quiz");
+		return "quiz";
+	}
+	@RequestMapping("/eduvideo")
+	public String eduvideo(Model model) {
+		System.out.println("eduvideo");
+		return "eduvideo";
+	}
 	
 }
