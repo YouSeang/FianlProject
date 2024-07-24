948c7877556b4e0d511c92a7757ae6146ca10e67
BIT <parksomi31>
2024-07-24 17:48:38 +0900

    test

--- src/main/java/kr/soft/study/controller/mypageController.java
+++ src/main/java/kr/soft/study/controller/mypageController.java
@@ -18,22 +18,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class mypageController {
 
 	private static final Logger logger = LoggerFactory.getLogger(mypageController.class);
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-
+	
 	@RequestMapping("/mypage")
 	public String mypage(Locale locale, Model model) {
 
 		return "mypage/mypage";
 	}
 
-	@RequestMapping("/couponshop")
-	public String couponshop(Locale locale, Model model) {
-
-		return "mypage/couponshop";
-	}
+	/*
+	 * @RequestMapping("/couponshop") public String couponshop(Locale locale, Model
+	 * model) {
+	 * 
+	 * return "mypage/couponshop"; }
+	 */
 
 	@RequestMapping("/modify")
 	public String modify(Locale locale, Model model) {
