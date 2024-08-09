a4e82701e9ffe7945a0ce87e76c6b5adbf7602d4
angstar <ysangstar@gmail.com>
2024-08-09 17:34:20 +0900

    seang

---
+++ src/main/java/kr/soft/study/controller/GameController.java
@@ -0,0 +1,86 @@
+package kr.soft.study.controller;
+
+import kr.soft.study.command.PointsCommand;
+import kr.soft.study.dto.Game;
+import kr.soft.study.dto.UserDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/game") // 클래스 레벨의 매핑
+public class GameController {
+
+    @Autowired
+    private PointsCommand pointsCommand;
+
+    @RequestMapping(value = "/game", method = RequestMethod.GET)
+    public String getGame(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        Game game = (Game) session.getAttribute("game");
+
+        if (game == null) {
+            game = new Game();
+            session.setAttribute("game", game);
+        }
+
+        request.setAttribute("game", game);
+        return "game/game"; // 뷰 이름을 반환
+    }
+
+    @RequestMapping(value = "/game", method = RequestMethod.POST)
+    public String postGame(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        Game game = (Game) session.getAttribute("game");
+
+        if (game == null) {
+            game = new Game();
+            session.setAttribute("game", game);
+        }
+
+        String action = request.getParameter("action");
+        if ("reset".equals(action)) {
+            game.reset();
+        }
+
+        session.setAttribute("game", game);
+        return "redirect:/game/game"; // 뷰로 리다이렉트
+    }
+
+    @RequestMapping(value = "/end", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, String> endGame(@RequestBody Map<String, Object> gameData, HttpSession session) {
+        UserDto user = (UserDto) session.getAttribute("user");
+        Map<String, String> response = new HashMap<>();
+
+        if (user == null || user.getUser_id() == null) {
+            response.put("message", "User is not logged in.");
+            return response;
+        }
+
+        String userId = user.getUser_id();
+        int gameScore = ((Integer) gameData.get("score")).intValue();
+        int pointsEarned = gameScore * 5; // 1점당 5포인트
+        String pointReason = "범죄자 잡기 게임";
+
+        pointsCommand.updatePoints(userId, pointsEarned, pointReason, 0); // 0은 quizId 대신 사용
+
+        response.put("message", "Game ended. Points updated.");
+        return response;
+    }
+}
